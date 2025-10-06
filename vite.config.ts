e3b7b96b121eff5b38e853b99a45b35ebc6809cd
Rakib Hossain <r.rakib.7254@gmail.com>
2025-10-06 17:03:21 +0600

    Fix Coinbase Wallet COOP header error

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'unsafe-none'
+    }
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
