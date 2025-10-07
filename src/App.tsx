906410c536c99707bb5eaf5542eb19cd74fd8fb0
Rakib Hossain <r.rakib.7254@gmail.com>
2025-10-07 16:27:55 +0600

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import { WagmiProvider } from 'wagmi'
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { config } from '@/config/wagmi'
-import { ThemeProvider } from '@/contexts/ThemeContext'
-import { SettingsProvider } from '@/contexts/SettingsContext'
 import { Header } from '@/components/Header'
 import { SwapPage } from '@/pages/SwapPage'
 import { PoolPage } from '@/pages/PoolPage'
@@ -14,25 +12,21 @@ const queryClient = new QueryClient()
 
 function App() {
   return (
-    <ThemeProvider>
-      <SettingsProvider>
-        <WagmiProvider config={config}>
-          <QueryClientProvider client={queryClient}>
-            <RainbowKitProvider>
-              <Router>
-                <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
-                  <Header />
-                  <Routes>
-                    <Route path="/" element={<SwapPage />} />
-                    <Route path="/pool" element={<PoolPage />} />
-                  </Routes>
-                </div>
-              </Router>
-            </RainbowKitProvider>
-          </QueryClientProvider>
-        </WagmiProvider>
-      </SettingsProvider>
-    </ThemeProvider>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <Router>
+            <div className="min-h-screen bg-gray-50">
+              <Header />
+              <Routes>
+                <Route path="/" element={<SwapPage />} />
+                <Route path="/pool" element={<PoolPage />} />
+              </Routes>
+            </div>
+          </Router>
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   )
 }
 
